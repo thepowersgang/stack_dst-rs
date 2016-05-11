9ada9df4d891825385e3974c4a51cb3a250a1c55
John Hodge <tpg+travis@mutabah.net>
2016-05-11 02:03:49 +0000

    rebuild pages at 4c9899d

--- target/doc/implementors/core/ops/trait.Drop.js
+++ target/doc/implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['stack_dst'] = ["impl&lt;T: ?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>, D: <a class='trait' href='stack_dst/trait.DataBuf.html' title='stack_dst::DataBuf'>DataBuf</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='stack_dst/struct.StackDSTA.html' title='stack_dst::StackDSTA'>StackDSTA</a>&lt;T, D&gt;",];
+implementors["stack_dst"] = ["impl&lt;T: ?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>, D: <a class='trait' href='stack_dst/trait.DataBuf.html' title='stack_dst::DataBuf'>DataBuf</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='stack_dst/struct.StackDSTA.html' title='stack_dst::StackDSTA'>StackDSTA</a>&lt;T, D&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
