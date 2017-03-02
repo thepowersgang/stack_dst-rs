76b92bdf6a0098f53b36843f5c4e1b8bcc96a8a0
John Hodge <tpg+travis@mutabah.net>
2017-03-02 08:02:15 +0000

    rebuild pages at 410b526

--- target/doc/implementors/core/ops/trait.DerefMut.js
+++ target/doc/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["stack_dst"] = ["impl&lt;T:&nbsp;?<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='trait core::marker::Sized'>Sized</a>, D:&nbsp;<a class='trait' href='stack_dst/trait.DataBuf.html' title='trait stack_dst::DataBuf'>DataBuf</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='trait core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='stack_dst/struct.StackDSTA.html' title='struct stack_dst::StackDSTA'>StackDSTA</a>&lt;T, D&gt;",];
+implementors["stack_dst"] = ["impl&lt;T:&nbsp;?<a class="trait" href="https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html" title="trait core::marker::Sized">Sized</a>, D:&nbsp;<a class="trait" href="stack_dst/trait.DataBuf.html" title="trait stack_dst::DataBuf">DataBuf</a>&gt; <a class="trait" href="https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html" title="trait core::ops::DerefMut">DerefMut</a> for <a class="struct" href="stack_dst/struct.StackDSTA.html" title="struct stack_dst::StackDSTA">StackDSTA</a>&lt;T, D&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
