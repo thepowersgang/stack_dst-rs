36005467c859117cd41eccf3f95b3fdf1f534290
John Hodge <tpg+travis@mutabah.net>
2020-01-23 10:14:31 +0000

    rebuild pages at e059cc8

--- target/doc/implementors/core/fmt/trait.Display.js
+++ target/doc/implementors/core/fmt/trait.Display.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["stack_dst"] = [{"text":"impl&lt;D:&nbsp;<a class=\"trait\" href=\"stack_dst/trait.DataBuf.html\" title=\"trait stack_dst::DataBuf\">DataBuf</a>, T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"stack_dst/struct.ValueA.html\" title=\"struct stack_dst::ValueA\">ValueA</a>&lt;T, D&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,&nbsp;</span>","synthetic":false,"types":["stack_dst::value::ValueA"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- target/doc/implementors/core/marker/trait.Freeze.js
+++ target/doc/implementors/core/marker/trait.Freeze.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["stack_dst"] = [{"text":"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>, D&gt; Freeze for <a class=\"struct\" href=\"stack_dst/struct.ValueA.html\" title=\"struct stack_dst::ValueA\">ValueA</a>&lt;T, D&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: Freeze,&nbsp;</span>","synthetic":true,"types":["stack_dst::value::ValueA"]},{"text":"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>, D&gt; Freeze for <a class=\"struct\" href=\"stack_dst/struct.StackA.html\" title=\"struct stack_dst::StackA\">StackA</a>&lt;T, D&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: Freeze,&nbsp;</span>","synthetic":true,"types":["stack_dst::stack::StackA"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
