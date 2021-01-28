1b0869481f8d4a794a5e1facc35f59697a6ef1b3
John Hodge <tpg+travis@mutabah.net>
2021-01-28 19:13:04 +0000

    rebuild pages at 807e9d4

--- target/doc/implementors/core/default/trait.Default.js
+++ target/doc/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["stack_dst"] = [{"text":"impl&lt;T:&nbsp;?Sized, D:&nbsp;DataBuf&gt; Default for StackA&lt;T, D&gt;","synthetic":false,"types":[]}];
+implementors["stack_dst"] = [{"text":"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>, D:&nbsp;<a class=\"trait\" href=\"stack_dst/trait.DataBuf.html\" title=\"trait stack_dst::DataBuf\">DataBuf</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stack_dst/struct.StackA.html\" title=\"struct stack_dst::StackA\">StackA</a>&lt;T, D&gt;","synthetic":false,"types":["stack_dst::stack::StackA"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
